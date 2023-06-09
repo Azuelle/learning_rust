5fcb070921ac1933c256ab914558f6262b32e24f
Azuelle <tjk@live.cn>
2023-06-10 01:21:10 +0800

    Convert submodules to regular files

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 1_1__4___5____1_____4;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
